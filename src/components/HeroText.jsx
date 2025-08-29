348676fe59f8d38b7c740bac1bffbe2b3f22f5ee
Aawhan <114390114+aawhan0@users.noreply.github.com>
2025-08-29 21:32:25 +0530

    mobile view added.

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,31 +1,40 @@
-import { FlipWords } from "./FlipWords"
+import { FlipWords } from "./FlipWords";
+const words = ["Minimal", "Scalable", "Effective"]
+import { motion } from "framer-motion";
 
 const HeroText = () => {
   return (
     <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
-        {/* {Desktop View} */}
-        <div className="flex-col hidden md:flex c-space">
-            <h1 className="text-4xl font-medium">Hi, I'm Aawhan</h1>
-            <div className="flex flex-col items-start">
-                <p className="text-5xl font-medium text-neutral-300">
-                    Software engineer building intuitive experiences</p>
-                {/* <div> get flipword from aceternity</div> */}
-                <div>
-                    <FlipWords
-                        words = {["Minimal", "Scalable", "Effective"]}
-                        className= "font-black text-white text-8xl"
-                    />
-                </div>
-                <p className="text-4xl font-medium text-neutral-300">Web solutions</p>
-            </div>
-            
+      {/* Desktop View */}
+      <div className="flex-col hidden md:flex c-space">
+        <motion.h1 className="text-4xl font-medium">Hi, I'm Aawhan</motion.h1>
+        <div className="flex flex-col items-start">
+          <motion.p className="text-5xl font-medium text-neutral-300">
+            Software engineer building intuitive experiences
+          </motion.p>
+          <motion.div>
+            <FlipWords
+              words={["Minimal", "Scalable", "Effective"]}
+              className="font-black text-white text-8xl"
+            />
+          </motion.div>
+          <motion.p className="text-4xl font-medium text-neutral-300">Web solutions</motion.p>
         </div>
-        {/* {Mobile View} */}
-        {/* <div className="flex-col hidden md:flex c-space">
-            Hi, I'm Aawhan
-        </div> */}
       </div>
-  )
-}
 
-export default HeroText
+      {/* Mobile View */}
+      <div className="flex flex-col space-y-6 md:hidden">
+        <motion.p className="text-4xl font-medium">Hi, I'm Aawhan</motion.p>
+        <p className="text-5xl font-black text-neutral-300">Building</p>
+        <motion.div>
+          <FlipWords words={words}
+          className="font-bold text-white text-7xl"
+          />
+        </motion.div>
+        <motion.p className="text-4xl font-black">Web Applications</motion.p>
+      </div>
+    </div>
+  );
+};
+
+export default HeroText;
