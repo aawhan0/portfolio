e4f65696a81e00088700f61032eabe9e1b3c34a2
Aawhan <114390114+aawhan0@users.noreply.github.com>
2025-08-31 11:59:18 +0530

    day4.0

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,13 +1,26 @@
+import {Canvas} from "@react-three/fiber"
+import {Astronaut} from "../components/Astronaut"
 import HeroText from "../components/HeroText"
 import ParallaxBackground from "../components/ParallaxBackground"
+import { OrbitControls } from "@react-three/drei"
+import { useMediaQuery } from "react-responsive"
 
 const Hero = () => {
+  const isMobile = useMediaQuery({maxWidth: 853}
+  )
   return (
     <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
         <HeroText/>
         <ParallaxBackground/>
         <figure className="absolute inset-0"
-        style={{width: "100vw", height: "100vh"}}>
+        style={{width: "100vw", height: "100vh"}}
+        >
+            <Canvas camera={{position:[0,1,3]}}>
+                <Astronaut
+                scale={isMobile && 0.23}
+                position={isMobile && [0, -1.5, 0]}/>
+                <OrbitControls />
+            </Canvas>
 
         </figure>
     </section>
