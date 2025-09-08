73ac20182665504ee14774d8639435bf04bdccac
Aawhan <114390114+aawhan0@users.noreply.github.com>
2025-09-08 19:52:20 +0530

    rollback

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import React from 'react'
-import Navbar from './sections/Navbar'
-import Hero from './sections/Hero'
-import About from './sections/About'
+import React from 'react';
+import Navbar from './sections/Navbar';
+import Hero from './sections/Hero';
+import About from './sections/About';
 import SplashCursor from './components/SplashCursor';
 
-return (
-    <>
-      <SplashCursor /> {/* Background water ripple effect */}
-      <div className="container mx-auto max-w-7xl" style={{ position: 'relative', zIndex: 0 }}>
-        <Navbar />
-        <Hero />
-        <About />
-        <section className="min-h-screen" />
-        <section className="min-h-screen" />
-        <section className="min-h-screen" />
-        <section className="min-h-screen" />
-        {/* about */}
-        {/* projects */}
-        {/* experience */}
-        {/* contact */}
-        {/* footer */}
-      </div>
-    </>
+const App = () => {
+  return (
+    <div className="container mx-auto max-w-7xl">
+      <SplashCursor /> {/* Cursor ripple effect applied globally */}
+      <Navbar />
+      <Hero />
+      <About />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      {/* about */}
+      {/* projects */}
+      {/* experience */}
+      {/* contact */}
+      {/* footer */}
+    </div>
   );
-;
+};
 
 export default App;
