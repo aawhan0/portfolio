8e9950cd185bf5f59eca3da7fdb8a48bcd83858a
Aawhan <114390114+aawhan0@users.noreply.github.com>
2025-09-08 19:39:09 +0530

    splash cursor added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
 import About from './sections/About'
+import SplashCursor from './components/SplashCursor';
 
 const App = () => {
   return <div className='container mx-auto max-w-7xl'>
+    <SplashCursor /> {/* Cursor ripple effect applied globally */}
     <Navbar />
     <Hero />
     <About />
