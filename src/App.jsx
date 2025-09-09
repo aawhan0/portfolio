c9514101cb9097d4586235cafc2971175ec0b208
Aawhan <114390114+aawhan0@users.noreply.github.com>
2025-09-09 15:10:34 +0530

    idk tried but the spotlight card isnt working rn

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,22 @@ import Navbar from './sections/Navbar';
 import Hero from './sections/Hero';
 import About from './sections/About';
 import SplashCursor from './components/SplashCursor';
+import GooeyNav from './components/GooeyNav';
+
+const navItems = [
+  { label: 'Home', href: '#home' },
+  { label: 'About', href: '#about' },
+  { label: 'Projects', href: '#projects' },
+  { label: 'Contact', href: '#contact' },
+];
+
+
 
 const App = () => {
   return (
     <div className="container mx-auto max-w-7xl">
       <SplashCursor /> {/* Cursor ripple effect applied globally */}
+      {/* <GooeyNav items={navItems} /> Updated navbar here */}
         <div className="container mx-auto max-w-7xl" style={{ position: 'relative', zIndex: 0 }}>
           {/* Your cards and page content here */}
         </div>
