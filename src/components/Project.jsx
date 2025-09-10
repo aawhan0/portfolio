6d57e77376ea2a9c226456f482e5212c2a44d59b
Aawhan <114390114+aawhan0@users.noreply.github.com>
2025-09-10 21:00:54 +0530

    ..

---
+++ src/components/Project.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Project = ({ title, description, subdescription, href, image, tags }) => {
+  return (
+    <>
+      <div className='flex-wrap items-center justify-between py-10 space-y-14 sm:flex sm:space-y-0'>
+        <p className='text-2xl'>{title}</p>
+        <div className='flex gap-5 mt-2 text-sand'>
+          {tags.map((tag, index) => (
+            <span key={index}>{tag.name || tag}</span>
+          ))}
+        </div>
+        <button className='flex items-center gap-1 cursor-pointer hover-animation' onClick={() => window.open(href, '_blank')}>
+          <img src="assets/arrow-right.svg" className="w-5" alt="arrow icon" />
+        </button>
+      </div>
+      <div className='bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full'></div>
+    </>
+  )
+}
+
+
+export default Project
---
+++ src/sections/Projects.jsx
@@ -0,0 +1,19 @@
+import { section } from 'framer-motion/client';
+import Project from '../components/Project';
+import { myProjects } from '../constants';
+
+const Projects = () => {
+  return (
+    <section className='relative c-space section-spacing'>
+        <h2 className='text-heading'>My Projects</h2>
+        <div className='bg-gradient-to-r from-transparent via-neutral-700 to-transparent mt-12 h-[1px] w-full'>
+        </div>
+            {myProjects.map((project) => (
+                <Project key={project.id} {...project}/>
+            ))}
+
+    </section>
+  )
+}
+
+export default Projects
